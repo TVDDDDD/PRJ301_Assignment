04c7e4de924d9a62298e9b11b548247d4cc499d5
TVDDDDD <hdand3007@gmail.com>
2025-07-18 10:11:44 +0700

    declare variables in User class
    declare variables and add getter, setter

--- EmployeeLeaveSystem/src/java/model/User.java
+++ EmployeeLeaveSystem/src/java/model/User.java
@@ -9,5 +9,59 @@ package model;
  * @author ACER
  */
 public class User {
-    
+
+    private String username;
+    private String password; // Plain text for simplicity; use hashed passwords in production
+    private String role; // e.g., Nhân viên, Trưởng phòng
+    private String department; // e.g., IT
+    private String manager; // Username of the user's manager
+
+    public User(String username, String password, String role, String department, String manager) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.department = department;
+        this.manager = manager;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
 }
