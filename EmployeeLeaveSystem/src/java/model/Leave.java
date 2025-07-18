0a37f6bd72376c1615376817fbc46fadf65348c8
TVDDDDD <hdand3007@gmail.com>
2025-07-18 10:12:13 +0700

    declare variables in Leave class
    declare variables and add getter, setter

--- EmployeeLeaveSystem/src/java/model/Leave.java
+++ EmployeeLeaveSystem/src/java/model/Leave.java
@@ -4,10 +4,86 @@
  */
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author ACER
  */
 public class Leave {
-    
+
+    private int id;
+    private String userUsername;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String reason;
+    private String status; // Inprogress, Approved, Rejected
+    private String processedBy; // Username of the manager who processed the request
+
+    public Leave(int id, String userUsername, LocalDate startDate, LocalDate endDate, String reason, String status, String processedBy) {
+        this.id = id;
+        this.userUsername = userUsername;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.reason = reason;
+        this.status = status;
+        this.processedBy = processedBy;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserUsername() {
+        return userUsername;
+    }
+
+    public void setUserUsername(String userUsername) {
+        this.userUsername = userUsername;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getProcessedBy() {
+        return processedBy;
+    }
+
+    public void setProcessedBy(String processedBy) {
+        this.processedBy = processedBy;
+    }
+
 }
